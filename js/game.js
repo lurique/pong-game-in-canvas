913612429e37d9a76a9eb10790dde9dda9a72baa
Lucas Henrique <lucashenrique.he@gmail.com>
2017-04-25 00:06:23 -0300

    Refactoring

--- js/game.js
+++ js/game.js
@@ -2,44 +2,28 @@ var App = App || {};
 
 ;(function() {
 
-	App.Game = {
-	
-		player1: 40,
-		player2: 40,
-		points: 10,
-		rounds: 100,
-		ballx: 50,
-		bally: 50,
-		ballDimension: 5,
-
+	App.Pong = {
 
 		init: function() {
-			this.createGame();
+			this.setCanvas();
+			this.prepGame();
 		},
 
-		createGame: function() {
+		setCanvas: function() {
 			this.canvas = document.getElementById('canvas');
 			this.ctx = this.canvas.getContext('2d');
-
-			setInterval(update, 1000/30);
-
-			function update() {
-				ballx+=xv;
-				bally+=yv;
-
-				if ( bally < 0 && yv < 0 ) {
-					yv=-yv;
-				}
-
-				ctx.fillStyle = '#000';
-				ctx.fillRect(0, 0, canvas.width, canvas.height);
-				ctx.fillStyle = '#FFF';
-				ctx.fillRect(0, player1, points, player2);
-				ctx.fillRect(canvas.width-points, player2, points, rounds);
-				ctx.fillRect(ballx-ballDimension/2, bally-ballDimension/2, ballDimension, ballDimension);
-			}
 		},
 
+		prepGame: function() {
+			var self = App.Pong;
+
+			self.ctx.fillStyle = "#000";
+		}
+
 	}
+
+	document.addEventListener('DOMContentLoaded', function() {
+		App.Pong.init();
+	});
 
 })();
