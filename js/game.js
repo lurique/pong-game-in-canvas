eafb5e8804344840fe03540681e2ecd69448ad2a
Lucas Henrique <lucashenrique.he@gmail.com>
2017-05-02 17:00:33 -0300

    Adding more elements on canvas

--- js/game.js
+++ js/game.js
@@ -2,64 +2,87 @@ var App = App || {};
 
 ;(function() {
 
-	'use strict';
-
 	App.Pong = {
 
-		// Common variables
-		width: window.innerWidth,
-		height: window.innerHeight,
-
-		// Pong elements
-		player1: '',
-		player2: '',
-		ballX: 10,
-		ballY: 10,
-
 		init: function() {
-			this.cacheSelectors();
-			this.setModules();
+			this.defineVariables();
+			this.defineModules();
+			this.defineEvents();
 		},
 
-		cacheSelectors: function() {
+		defineVariables: function() {
+			// Variables
 			this.canvas = document.getElementById('canvas');
 			this.ctx = this.canvas.getContext('2d');
+
+			this.player1Score = 0;
+			this.player2Score = 0;
+
+			// Constants
+			this.FRAMES = 30;
+			this.WIN_SCORE = 5;
+			this.VICTORY = false;
+			this.VERSUS = false;
 		},
 
-		setModules: function() {
+		defineModules: function() {
 			window.onload = function() {
 				var self = App.Pong;
 
-				self.Modules.buildArena();
-
-				setInterval(self.Modules.buildArena, 10);
+				self.Modules.drawArena();
 			}
 		},
 
 		Modules: {
-			buildArena: function() {
+			drawArena: function() {
 				var self = App.Pong;
 
-				self.ballX += 1;
-
-				// Build the arena background
+				// Draw the background
 				self.ctx.fillStyle = '#000';
 				self.ctx.fillRect(0, 0, self.canvas.width, self.canvas.height);
 
-				// Build division of the arena
+				// Draw arena division
 				self.ctx.fillStyle = '#FFF';
 				self.ctx.fillRect(self.canvas.width / 2, 0, 1, self.canvas.height);
 
-				// Build goal area
-				self.ctx.strokeStyle = '#FFF';
-				self.ctx.strokeRect(-2, self.canvas.height / 4, 80, self.canvas.height / 2);
-				self.ctx.strokeStyle = '#FFF';
-				self.ctx.strokeRect(self.canvas.width - 78, self.canvas.height / 4, 80, self.canvas.height / 2);
 
-				// Create ball
-				self.ctx.fillStyle = '#FFF';
-				self.ctx.fillRect(self.ballX, self.ballX, 5, 5);
-			}
+			},
+		},
+
+		defineEvents: function() {
+			
+		},
+
+		Events: {
+			resetBall: function() {
+				var self = App.Pong;
+
+				if ( self.player1Score >= self.WIN_SCORE || self.player2Score >= self.WIN_SCORE ) {
+					self.VICTORY = true;
+				}
+
+				self.ballSpeedX = -self.ballSpeedX;
+				self.ballX = self.canvas.width / 2;
+				self.ballY = self.canvas.height / 2;
+			},
+
+			startCPU: function() {
+				var self = App.Pong;
+
+				var paddleCenter = self.paddle2Y + (self.PADDLE_HEIGHT / 2);
+
+				if ( paddleCenter < self.ballY - 35 ) {
+					paddle2Y = paddle2Y + 6;
+				} else if ( paddleCenter > ballY + 35 ) {
+					paddle2Y = paddle2Y - 6;
+				}
+			},
+
+			startMultiplayer: function() {
+				var self = App.Pong;
+
+
+			},
 		}
 
 	}
